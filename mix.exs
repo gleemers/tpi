bd21d7a1d2088080b704b833e90af1de3a72c69f
Thoq <125930852+Thoq-jar@users.noreply.github.com>
2025-03-06 14:16:51 +0000

    init stuff, TODO: fix some borking with escript

--- mix.exs
+++ mix.exs
@@ -1,29 +1,24 @@
-defmodule Tpi.MixProject do
+defmodule Tpi.Umbrella.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :tpi,
+      apps_path: "apps",
       version: "0.1.0",
-      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      escript: [main_module: Main],
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp aliases do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # run tests for all apps
+      test: ["test --no-start"]
     ]
   end
 end
